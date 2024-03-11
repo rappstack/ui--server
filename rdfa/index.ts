bbc47cdf26bae90d3728721cd17d7dd9db6f5586
Brian Takita <brian.takita@gmail.com>
2024-03-10 23:25:56 -0400

    ./rdfa:
    	schema_org_WebPage_id__link_→schema_org_WebPage_id__link_a1_:
    		+ ∋ schema_org_rdfa__link_: @id: ∋ WebPage_id_
    		∋ schema_org_rdfa__link_: isPartOf: fix: ∋ WebSite_id_
    	schema_org_Article_id__link_→schema_org_Article_id__link_a1_:
    		+ ∋ schema_org_rdfa__link_: isPartOf: ∋ WebPage_id_

--- rdfa/index.ts
+++ rdfa/index.ts
@@ -1,3 +1,4 @@
+import { WebSite_id_ } from '@btakita/domain--server--briantakita/jsonld'
 import type { Article, Thing, WebPage } from '@btakita/schema-dts'
 import { WebPage_id_ } from '@rappstack/domain--server/jsonld'
 import { schema_org_props_rdfa_T } from '@rappstack/domain--server/rdfa'
@@ -9,12 +10,18 @@ import { link_ } from 'relementjs/html'
 export function schema_org_rdfa__link_<T extends Thing>(props:schema_org_props_rdfa_T<T>&{ href:string }) {
 	return link_(props)
 }
-export function schema_org_WebPage_id__link_(ctx:request_ctx_T) {
-  return schema_org_rdfa__link_<WebPage>({ property: 'isPartOf', href: WebPage_id_(ctx)})
+export function schema_org_WebPage_id__link_a1_(ctx:request_ctx_T) {
+  return [
+		schema_org_rdfa__link_<WebPage>({ property: '@id', href: WebPage_id_(ctx)}),
+		schema_org_rdfa__link_<WebPage>({ property: 'isPartOf', href: WebSite_id_(ctx)}),
+	]
 }
-export function schema_org_Article_id__link_(ctx:request_ctx_T) {
-	return schema_org_rdfa__link_<Article>({
-		property: '@id',
-		href: url__join(site__website_(ctx)!, request_url__pathname_(ctx), '#Article')
-	})
+export function schema_org_Article_id__link_a1_(ctx:request_ctx_T) {
+	return [
+		schema_org_rdfa__link_<Article>({
+			property: '@id',
+			href: url__join(site__website_(ctx)!, request_url__pathname_(ctx), '#Article')
+		}),
+		schema_org_rdfa__link_<Article>({ property: 'isPartOf', href: WebPage_id_(ctx)}),
+	]
 }
