448ccd485b7e36a706ce7635319c51cf489320bf
Brian Takita <brian.takita@gmail.com>
2024-03-17 03:42:19 -0400

    ./rdfa: + schema_org_rdfa__meta_
    update dependencies

--- rdfa/index.ts
+++ rdfa/index.ts
@@ -4,3 +4,6 @@ import type { Thing } from 'schema-dts'
 export function schema_org_rdfa__link_<T extends Thing>(props:schema_org_props_rdfa_T<T>&{ href:string }) {
 	return link_(props)
 }
+export function schema_org_rdfa__meta_<T extends Thing>(props:schema_org_props_rdfa_T<T>&{ content:string }) {
+	return link_(props)
+}
