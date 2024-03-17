cf02bb419a60e02e07e0f6a88f2bcdfaa1815717
Brian Takita <brian.takita@gmail.com>
2024-03-16 21:03:58 -0400

    ./rdfa: - schema_org_Article__link_a1_

--- rdfa/index.ts
+++ rdfa/index.ts
@@ -1,17 +1,6 @@
-import type { Article, Thing } from 'schema-dts'
-import { WebPage_id_ } from '@rappstack/domain--server/jsonld'
-import { Article_id_, type schema_org_props_rdfa_T } from '@rappstack/domain--server/rdfa'
+import { type schema_org_props_rdfa_T } from '@rappstack/domain--server/rdfa'
 import { link_ } from 'relementjs/html'
-import { type request_ctx_T } from 'relysjs/server'
+import type { Thing } from 'schema-dts'
 export function schema_org_rdfa__link_<T extends Thing>(props:schema_org_props_rdfa_T<T>&{ href:string }) {
 	return link_(props)
-}
-export function schema_org_Article__link_a1_(ctx:request_ctx_T) {
-	return [
-		schema_org_rdfa__link_<Article>({
-			property: '@id',
-			href: Article_id_(ctx)
-		}),
-		schema_org_rdfa__link_<Article>({ property: 'isPartOf', href: WebPage_id_(ctx)}),
-	]
 }
