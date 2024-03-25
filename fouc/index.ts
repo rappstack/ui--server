cb49d7fe5c9f8ba18b86bf5951a998b6b13757a9
Brian Takita <brian.takita@gmail.com>
2024-03-25 11:29:22 -0400

    ./fouc:
    	fouc__remove__script_→fouc__remove__fragment_
    	fouc__remove__script_props_T→fouc__remove__fragment_props_T

--- fouc/index.ts
+++ fouc/index.ts
@@ -1,16 +1,21 @@
 import { raw_ } from 'relementjs'
-import { script_ } from 'relementjs/html'
-export function fouc__remove__script_($p:fouc__remove__script_props_T) {
+import { script_, style_ } from 'relementjs/html'
+export function fouc__remove__fragment_($p:fouc__remove__fragment_props_T) {
 	let { dark_bg_color, light_bg_color, reset_timeout } = $p
 	reset_timeout ??= 1000
-	return (
+	return [
+		style_({ type: 'text/css' }, raw_(
+			// language=css
+			`body{background-color:rgb(${light_bg_color});}@media(prefers-color-scheme:dark){body{background-color:rgb(${dark_bg_color});}}`
+		)),
 		script_({ type: 'module' }, raw_(
 			// remove fouc
 			/// language=js
 			`let t=localStorage.getItem('theme')??window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light';let h=document.firstElementChild;h.setAttribute('data-theme',t);h.style.backgroundColor='rgb('+(t==='dark'?'${dark_bg_color}':'${light_bg_color}')+')';setTimeout(()=>h.style.backgroundColor='',${reset_timeout})`
-		)))
+		))
+	]
 }
-export type fouc__remove__script_props_T = {
+export type fouc__remove__fragment_props_T = {
 	dark_bg_color:string
 	light_bg_color:string
 	reset_timeout?:number
