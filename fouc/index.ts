5a9583416107b80d43f803b7e45a02e5e16894c4
Brian Takita <brian.takita@gmail.com>
2024-03-27 01:41:38 -0400

    ./fouc:
    	fouc__remove__fragment_props_T→nofouc__remove__fragment_props_T
    	fouc__remove__fragment_→nofouc__theme__set__fragment_
    	+ nofouc__body_bg__script_props_T
    	+ nofouc__body_bg__script_

--- fouc/index.ts
+++ fouc/index.ts
@@ -1,22 +1,34 @@
 import { raw_ } from 'relementjs'
 import { script_, style_ } from 'relementjs/html'
-export function fouc__remove__fragment_($p:fouc__remove__fragment_props_T) {
+type nofouc__remove__fragment_props_T = {
+	dark_bg_color:string
+	light_bg_color:string
+	reset_timeout?:number
+}
+export function nofouc__theme__set__fragment_($p:nofouc__remove__fragment_props_T) {
 	let { dark_bg_color, light_bg_color, reset_timeout } = $p
 	reset_timeout ??= 1000
 	return [
 		style_({ type: 'text/css' }, raw_(
 			// language=css
-			`html{background-color:rgb(${light_bg_color});}@media(prefers-color-scheme:dark){body{background-color:rgb(${dark_bg_color});}}`
+			`html{background-color:rgb(${light_bg_color});}@media (prefers-color-scheme: dark){body{background-color:rgb(${dark_bg_color});}}html.nofouc .nofouc{opacity:0;}`
 		)),
-		script_({ type: 'module' }, raw_(
+		script_(raw_(
 			// remove fouc
 			/// language=js
-			`let t=localStorage.getItem('theme')??window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light';let h=document.firstElementChild;h.classList.add(t);h.setAttribute('data-theme',t);h.style.backgroundColor='rgb('+(t==='dark'?'${dark_bg_color}':'${light_bg_color}')+')';setTimeout(()=>h.style.backgroundColor='',${reset_timeout})`
+			`let t=globalThis.__theme=localStorage.getItem('theme')??window.matchMedia('(prefers-color-scheme: dark)').matches?'dark':'light';let h=document.firstElementChild;h.classList.add(t);h.setAttribute('data-theme',t);h.style.backgroundColor='rgb('+(t==='dark'?'${dark_bg_color}':'${light_bg_color}')+')';setTimeout(()=>h.style.backgroundColor='',${reset_timeout});`
 		))
 	]
 }
-export type fouc__remove__fragment_props_T = {
+type nofouc__body_bg__script_props_T = {
 	dark_bg_color:string
 	light_bg_color:string
-	reset_timeout?:number
+}
+export function nofouc__body_bg__script_($p:nofouc__body_bg__script_props_T) {
+	const {
+		dark_bg_color,
+		light_bg_color,
+	} = $p
+	return script_({ type: 'application/javascript' }, raw_(
+		`document.body.style.backgroundColor='rgb('+(globalThis.__theme==='dark'?'${dark_bg_color}':'${light_bg_color}')+')';setTimeout(()=>document.body.style.backgroundColor=undefined,1000)`))
 }
