c3a1b8015496df2ccaab5df36af4b8cf6831df09
Brian Takita <brian.takita@gmail.com>
2024-04-21 05:09:02 -0400

    + ./md:
    	+ app_marked_
    	+ app_marked__set
    
    + ∋ marked

---
+++ md/app_marked.ts
@@ -0,0 +1,10 @@
+import { ns_id_be_sig_triple_ } from 'ctx-core/rmemo'
+import { type Marked } from 'marked'
+export const [
+	,
+	app_marked_,
+	app_marked__set,
+] = ns_id_be_sig_triple_(
+	'app',
+	'app_marked',
+	()=><Marked|undefined>undefined)
